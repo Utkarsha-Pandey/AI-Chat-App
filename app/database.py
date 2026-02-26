a727d806354015f662485f5d083f807191b021d4
Utkarsha-Pandey <utkarshapandey2004@gmail.com>
2026-02-26 17:08:09 +0530

    minor changes done

--- app/database.py
+++ app/database.py
@@ -2,17 +2,28 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
+#from sqlalchemy.exc import OperationalError
 
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    pool_pre_ping=True  
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker( autocommit=False, autoflush=False, bind=engine)
+
+# def test_connection():
+#     try:
+#         with engine.connect() as connection:
+#             print("✅ Database connected successfully!")
+#     except OperationalError as e:
+#         print("❌ Database connection failed!")
+#         print(e)
+
+# test_connection()
+
 
 Base = declarative_base()
 
