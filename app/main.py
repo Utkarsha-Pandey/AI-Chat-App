ff8108c294d3e5dd297823603c45c74c8b5d11ea
Utkarsha-Pandey <utkarshapandey2004@gmail.com>
2026-03-01 16:56:56 +0530

    added secure user registration endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Depends
+from app import auth
+from fastapi import FastAPI, Depends,status, HTTPException
 from sqlalchemy.orm import Session
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -11,9 +12,16 @@ app = FastAPI()
 def read_root():
     return {"message": " Welcome, Utkarsha."}
 
-@app.post("/test-user/")
-def create_test_user(email: str, password: str, db: Session = Depends(get_db)):
-    new_user = models.User(email=email, hashed_password=password) 
+@app.post("/users/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    
+    existing_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    hashed_pwd = auth.get_password_hash(user.password)
+
+    new_user = models.User(email=user.email, hashed_password=hashed_pwd)
     
     db.add(new_user)
     db.commit()
